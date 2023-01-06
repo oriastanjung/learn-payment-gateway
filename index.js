74579bcdd6df26fe3752672f4c67badf942600ea
oriastanjung <oriastan999@gmail.com>
2023-01-06 15:32:47 +0700

    trying payment gateway

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = 8080;
+const midtransClient = require("midtrans-client");
+
+let snap = new midtransClient.Snap({
+  isProduction: false,
+  serverKey: "SB-Mid-server-cXKPVymqElZUX6mOXntttP88",
+});
+
+let parameter = {
+  transaction_details: {
+    order_id: "YOUR-ORDERID-123456",
+    gross_amount: 10000,
+  },
+  credit_card: {
+    secure: true,
+  },
+  customer_details: {
+    first_name: "budi",
+    last_name: "pratama",
+    email: "budi.pra@example.com",
+    phone: "08111222333",
+  },
+};
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  snap.createTransaction(parameter).then((transaction) => {
+    // transaction token
+    let transactionToken = transaction.token;
+    res.json({ data: transaction });
+    console.log("transactionToken:", transaction);
+  });
+  //   res.send("hello world");
+});
+
+app.listen(PORT, () =>
+  console.log(`ExpressApp listening on localhost port ${PORT}!`)
+);
